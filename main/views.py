acf59da5ec9e8e4deae6634c589a444001997885
GuilhermePaiva1 <guipaiva132@hotmail.com>
2023-09-28 08:36:31 -0300

    finish 2.0

--- main/views.py
+++ main/views.py
@@ -4,32 +4,16 @@ from django.views import generic
 from django.contrib.messages import views
 from .forms import ReservaForm
 from .models import Reserva
-# Create your views here.
 
-#def index(request):
-#    return render(request, 'index.html')
 class HomeView(generic.TemplateView):
     template_name = "main/index.html"
 
-#def listar_reserva(request):
- #   reservas = reserva.objects.all() 
-  #  context = {'reservas': reservas}
-   # return render(request, 'Listagem.html', context)
+
 class ReservasListView(generic.ListView):
     model = Reserva
     template_name = "main/Listagem.html"
 
-#def criar_reserva(request):
-#    if request.method == "POST":
-#
-#       form = reservaForm(request.POST)
-#        if form.is_valid():
-#            form.save()
-#            return redirect('listar')
-#    else: 
-#        form = reservaForm()
-#
-#    return render(request, 'reserva.html', {'form': form})
+
 class ReservaCreateView(views.SuccessMessageMixin,generic.CreateView):
     model = Reserva
     form_class = ReservaForm
@@ -38,19 +22,13 @@ class ReservaCreateView(views.SuccessMessageMixin,generic.CreateView):
     success_message = "Reserva criada com sucesso"
 
 
-#def remover(request, id):
-#    reserva_obj = get_object_or_404(reserva, id=id)  
-#    reserva_obj.delete()
-#    return redirect('listar')
+
 class ReservaDeleteView(views.SuccessMessageMixin, generic.DeleteView):
     model = Reserva
     success_url = reverse_lazy("main:Listagem")
     success_message = "Reserva deletada com sucesso"
 
-#def detalhe(request, id):
-#    reserva_obj = get_object_or_404(reserva, id=id) 
-#    context = {'reserva': reserva_obj}
-#    return render(request, 'detalhe_reserva.html', context)
+
 class ReservaDetailView(generic.DetailView):
     model = Reserva
 
