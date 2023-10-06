c3b6f6d4f8aaa694128934081c6f73622004f47f
Guilherme <guipaiva132@hotmail.com>
2023-10-06 17:38:43 -0300

    finish authentication

--- core/views.py
+++ core/views.py
@@ -3,13 +3,25 @@ from django.shortcuts import render
 from django.views import generic
 from main.models import Reserva
 from stands.models import Stand
+from django.contrib.messages import views
+from django.contrib import messages
 
 # Create your views here.
-class HomeView(LoginRequiredMixin, generic.TemplateView):
+class HomeView(LoginRequiredMixin, views.SuccessMessageMixin, generic.TemplateView):
     template_name = "pages/home.html"
+    success_message = "Login realizado com sucesso"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['num_reservas'] = Reserva.objects.count()
         context['num_stands'] = Stand.objects.count()
         return context
+        
+    def form_valid(self, form):
+        # Faça a autenticação do usuário aqui, se necessário
+        # Por exemplo, você pode usar o método 'form.get_user()' para obter o usuário autenticado
+
+        # Defina a mensagem de sucesso usando 'messages.success'
+        messages.success(self.request, 'Você foi autenticado com sucesso!')
+
+        return super().form_valid(form)
