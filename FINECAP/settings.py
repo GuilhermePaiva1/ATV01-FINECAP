da7e876f6aa400b753af72c964cd75b3fec4cc88
Guilherme <guipaiva132@hotmail.com>
2023-10-11 16:13:08 -0300

    finish auth

--- FINECAP/settings.py
+++ FINECAP/settings.py
@@ -86,7 +86,8 @@ AUTHENTICATION_BACKENDS = [
 WSGI_APPLICATION = "FINECAP.wsgi.application"
 AUTH_USER_MODEL = "users.User"
 LOGIN_REDIRECT_URL = "core:home"
-
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+ACCOUNT_EMAIL_VERIFICATION = "none"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
